4f3fb36dd0c180636fabcf022d4d22d4902dec1b
cheney09 <yanyongcheng@sina.cn>
2023-12-27 23:28:29 +0800

    add 【Spring实战】09 MyBatis Generator

---
+++ 09/demo/src/main/java/com/cheney/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.cheney.demo.controller;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("generator")
+    public void generator() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(this.getClass().getResourceAsStream("/generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
---
+++ 09/demo/src/main/java/com/cheney/demo/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.cheney.demo.dao;
+
+import com.cheney.demo.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User row);
+
+    int insertSelective(User row);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User row);
+
+    int updateByPrimaryKey(User row);
+}
